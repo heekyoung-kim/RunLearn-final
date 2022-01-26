9c7cbdd6d481b9ceb7a6235f3c6ecbc276fa8fb5
heekyoung-kim <kimheekyoung21@gmail.com>
2022-01-26 09:06:13 +0900

    카트기능 기초작업(mapper, service, jsp), 회원가입 유효성체크

---
+++ src/main/java/com/hta/lecture/web/restController/CartsRestController.java
@@ -0,0 +1,26 @@
+package com.hta.lecture.web.restController;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hta.lecture.dto.ResponseDto;
+import com.hta.lecture.service.UserService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/api")
+public class CartsRestController {
+
+	private UserService userService;
+	
+	@GetMapping("/carts")
+	public ResponseDto<?> addCarts(int no) {
+		ResponseDto<?> response = new ResponseDto<>();
+		
+		
+		return response;
+	}
+}

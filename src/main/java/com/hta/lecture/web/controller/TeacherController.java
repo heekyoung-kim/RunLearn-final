2499fa69b3f0c460607c6eb03eff669b8f69cb53
localhost <carbancle2@gmail.com>
2022-02-08 10:10:08 +0900

    TeacherController 수정

--- src/main/java/com/hta/lecture/web/controller/TeacherController.java
+++ src/main/java/com/hta/lecture/web/controller/TeacherController.java
@@ -1,8 +1,16 @@
 package com.hta.lecture.web.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hta.lecture.service.ClassService;
+import com.hta.lecture.vo.Classes;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,8 +19,19 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/instructor")
 public class TeacherController {
 	
-	@GetMapping("")
-	public String insturctorMyPage() {
+	static final Logger logger = LogManager.getLogger(ClassController.class);
+	
+	@Autowired
+	ClassService classService;
+	
+	@GetMapping("{no}")
+	public String insturctorMyPage(@PathVariable(name = "no") int no, Model model) {
+		
+		log.info("조회할 강의번호: " + no);
+		Classes classes = classService.getClassDetail(no);
+		
+		model.addAttribute("classes", classes);
+		
 		return "teacher-mypage/home/teacherDashboard";
 	}
 	@GetMapping("/profile")

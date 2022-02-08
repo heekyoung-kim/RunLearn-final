64299a6ea9017688fb53095c0f4301e1fb6f4f82
heekyoung-kim <kimheekyoung21@gmail.com>
2022-02-08 09:23:40 +0900

    dto에 쿠폰마감기한추가, 쿠폰추가기능구현, 쿠폰d-day기능구현.

--- src/main/java/com/hta/lecture/dto/UserCouponDto.java
+++ src/main/java/com/hta/lecture/dto/UserCouponDto.java
@@ -1,6 +1,12 @@
 package com.hta.lecture.dto;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,7 +28,23 @@ public class UserCouponDto {
 	private String useStatus;
 	private Date useDate;
 	private String periodStatus; 
+	@JsonFormat(pattern = "yyyy년 M월 d일 HH:mm:ss")
 	private Date pubDate;
+	@JsonFormat(pattern = "yyyy년 M월 d일 HH:mm:ss")	
+	private Date periodDate;
+	
+	// 남은 유효기간.
+	public long getRemainDate() {
+		Date period = this.getPeriodDate(); // 유효기간
+		Date now = new Date(); // 현재날짜
+		long remainDate; // 남은기간
+		
+		if(period.getTime() < now.getTime()) {
+			remainDate = 0;
+		}
+		remainDate = (period.getTime() - now.getTime())/(24*60*60*1000); // 일수 계산.
+		return remainDate;
+	}
 }
 
 

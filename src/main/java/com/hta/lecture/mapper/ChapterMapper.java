984db4296c62087b79a22a7b3bf2c22c6fc5340a
localhost <carbancle2@gmail.com>
2022-02-14 14:59:02 +0900

    학생 - 내학습 페이지 작업

---
+++ src/main/java/com/hta/lecture/mapper/ChapterMapper.java
@@ -0,0 +1,17 @@
+package com.hta.lecture.mapper;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.hta.lecture.dto.ChapterDto;
+import com.hta.lecture.vo.ClassDetail;
+
+@Mapper
+public interface ChapterMapper {
+	
+	List<ChapterDto> getChapterByClassNo(int no);
+	
+	List<ClassDetail> getClassDetailByChapterNo(int no);
+	
+}
